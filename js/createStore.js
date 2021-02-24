2f2fbf4d309183afbc34842e750cbf79a97fb12f
branmar97 <brandonmar97@gmail.com>
2021-02-24 18:04:11 -0500

    commit tree 6999a0b6ed57cdbbac24862894f9f5201157a073

--- js/createStore.js
+++ js/createStore.js
@@ -16,19 +16,15 @@ function createStore(reducer) {
   };
 }
 
-let store = createStore()
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
 
-store.dispatch({ type: '@@INIT' })
-
-// function reducer(state = { count: 0 }, action) {
-//   switch (action.type) {
-//     case 'INCREASE_COUNT':
-//       return { count: state.count + 1 };
-
-//     default:
-//       return state;
-//   }
-// };
+    default:
+      return state;
+  }
+};
 
 function render() {
   let container = document.getElementById('container');
@@ -40,3 +36,7 @@ let button = document.getElementById('button');
 button.addEventListener('click', function() {
     store.dispatch({ type: 'INCREASE_COUNT' });
 })
+
+let store = createStore(reducer)
+
+store.dispatch({ type: '@@INIT' })
